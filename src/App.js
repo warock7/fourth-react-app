ced8fa0dab20d9013311f2ac0d5bd5f514cf0400
warock7 <wr00009@mix.wvu.edu>
2023-11-13 13:28:21 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./styles.css";
 
 function App() {
+  const [task, setTask] = useState([]);
+  const inputRef = useRef(null);
+
+  const addItem = (e) => {
+    e.preventDefault();
+    const newItem = inputRef.current.value;
+    if (newItem !== "") {
+      setTask([...task, newItem]);
+    }
+    inputRef.current.value = "";
+  };
+
+  const removeItem = (index) => {
+    const updatedTasks = task.filter((_, i) => i !== index);
+    setTask(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Navbar /> */}
+      <div className="App container">
+        <h1>ToDo List</h1>
+        <input type="text" placeholder="Enter an item" ref={inputRef} />
+        <ul>
+          {task.map((task, index) => (
+            <li className="li-item" key={index}>
+              {task}
+              <span className="text-dark" onClick={() => removeItem(index)}>
+                x
+              </span>
+            </li>
+          ))}
+        </ul>
+        <button className="custom-btn" onClick={addItem}>
+          Add
+        </button>
+      </div>
+    </>
   );
 }
 
